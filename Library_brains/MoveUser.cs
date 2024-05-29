78adbbae9eecdb568e2d12828ff0f05bc70f72d2
Александр Туйгунов <tvsasha2006@gmail.com>
2024-05-30 00:59:08 +0500

    добавлены новые методы

--- Library_brains/MoveUser.cs
+++ Library_brains/MoveUser.cs
@@ -10,28 +10,6 @@ namespace Library_brains
 {
     internal class MoveUser
     {
-        //private string _filePath = "users.json";
-        //public void Register(User _user)
-        //{
-        //    _user = user ?? new User();
-        //    DataContext = _user;
-
-        //    if (_user != null)
-        //    {
-        //        txtFullName.Text = _user.FullName;
-        //        txtEmail.Text = _user.Email;
-        //        txtPhone.Text = _user.Phone;
-        //    }
-
-        //    if (System.IO.File.Exists(_filePath))
-        //    {
-        //        var json = System.IO.File.ReadAllText(_filePath);
-        //        _users = JsonSerializer.Deserialize<List<User>>(json) ?? new List<User>();
-        //    }
-        //    else
-        //    {
-        //        _users = new List<User>();
-        //    }
-        //}
+        
     }
 }
